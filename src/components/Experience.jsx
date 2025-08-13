b897478364e05138cdc527391a504620075dfcbd
bartwisch <123270793+bartwisch@users.noreply.github.com>
2025-08-13 16:42:34 +0200

    Refactor Experience component to add two cubes and lighting

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,10 +4,22 @@ export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
+      
+      {/* First cube */}
+      <mesh position={[-2, 0, 0]}>
         <boxGeometry />
         <meshNormalMaterial />
       </mesh>
+      
+      {/* Second cube */}
+      <mesh position={[2, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+      
+      {/* Add some lighting for the standard material */}
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
     </>
   );
 };
